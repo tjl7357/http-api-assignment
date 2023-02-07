525a410d3b28be5e88da007874fb6049e4ce4422
TJ Larson <tjl7357@rit.edu>
2023-02-07 18:28:50 -0500

    Got main functionality working, just need to add responses

---
+++ src/responses.js
@@ -0,0 +1,63 @@
+// Requires
+const fs = require('fs');
+
+// Get necessary files
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+
+// Responses Function
+const respond = (request, response, content, type, status) => {
+  response.writeHead(status, { 'Content-Type': type });
+  response.write(content);
+  response.end();
+};
+
+// Get Index
+const getIndex = (request, response) => {
+  respond(request, response, index, 'text/html', 200);
+};
+
+// Get Success
+const getSuccess = (request, response, acceptedTypes) => {
+  const responseData = {
+    message: 'This is a successful response',
+  };
+
+  if (acceptedTypes[0] === 'text/xml') {
+    let xmlString = '<response>';
+    xmlString += `<message>${responseData.message}</message>`;
+    xmlString += '</response>';
+    return respond(request, response, xmlString, acceptedTypes[0], 200);
+  }
+
+  return respond(request, response, JSON.stringify(responseData), 'application/json', 200);
+};
+
+// Get Bad Request
+const getBadRequest = (request, response, acceptedTypes, params) => {
+  if (!params.valid || params.valid !== 'true'){
+
+    const responseData = {
+      message: 'Missing valid query parameter set to true',
+      id: 'badRequest',
+    };
+
+    if (acceptedTypes[0] === 'text/xml') {
+      let xmlString = '<response>';
+      xmlString += `<message>${responseData.message}</message>`;
+      xmlString += `<id>${responseData.id}</id>`
+      xmlString += '</response>';
+      return respond(request, response, xmlString, acceptedTypes[0], 400);
+    }
+
+    return respond(request, response, JSON.stringify(responseData), 'application/json', 400);
+  }
+  
+  return getSuccess(request, response, acceptedTypes);
+}
+
+// Exports
+module.exports = {
+  getIndex,
+  getSuccess,
+  getBadRequest,
+};
---
+++ src/server.js
@@ -0,0 +1,35 @@
+// Requires
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const responseHandler = require('./responses.js');
+
+// Establish Port
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// URL Struct
+const urlStruct = {
+  '/': responseHandler.getIndex,
+  '/success': responseHandler.getSuccess,
+  '/badRequest': responseHandler.getBadRequest,
+  notFound: responseHandler.getIndex,
+};
+
+// Response Function
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  const acceptedTypes = request.headers.accept.split(',');
+  const params = query.parse(parsedUrl.query);
+
+  const func = urlStruct[parsedUrl.pathname];
+  if (func) {
+    func(request, response, acceptedTypes, params);
+  } else {
+    urlStruct.notFound(request, response, acceptedTypes, params);
+  }
+};
+
+// Create the server
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1: ${port}`);
+});
